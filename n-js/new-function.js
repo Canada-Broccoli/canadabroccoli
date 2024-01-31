8ffa908bcfe9e64a753ccc7004c85f9f27be8fa8
Canada-Broccoli <chen0969@algonquinlive.com>
2024-01-30 23:37:08 -0500

    fix rwd and header

--- n-js/new-function.js
+++ n-js/new-function.js
@@ -7,18 +7,18 @@ function aboutmePage(){window.open("aboutme.html");}
             function barWeb(){window.open("https://newmatch19.com/project/waroom_lady/");}
 
             $(window).scroll(function() {
-            bannerBall("svgB", "0.6", "1");
-            bannerBall("svgY", "1.1", "-1");
-            bannerBall("svgY2", "0.8", "0");
-            bannerBall("svgR", "0.7", "-1");
-            writerFun("introP")
-            writerFun("designP");
-            writerFun("animationP");
-            writerFun("photoP");
+            bannerBall("#svgB", "0.6", "1");
+            bannerBall("#svgY", "1.1", "-1");
+            bannerBall("#svgY2", "0.8", "0");
+            bannerBall("#svgR", "0.7", "-1");
+            writerFun("#introP");
+            writerFun("#designP");
+            writerFun("#animationP");
+            writerFun("#photoP");
             });
 
-            function writerFun(target) {
-            const targetP = $("#" + target);
+            function writerFun(content) {
+            const targetP = $(content);
             let scrollD = $(window).scrollTop();
             const targetPosition = targetP.offset().top;
             const windowHeight = $(window).height();
@@ -28,23 +28,41 @@ function aboutmePage(){window.open("aboutme.html");}
             }
             }
 
+            function bannerBall(targetBall, amount, layer) {
+                let scrollD = $(window).scrollTop();
+                const windowHeight = $(window).height();
+                const bannerHeight = $("#bBanner").offset().top + windowHeight;
+                const theBall = $(targetBall); // Update this line!!!!
+                const endDrop = $("#website").offset().top - windowHeight;
+                let ballDrop = scrollD * amount;
+                theBall.css("transition", "1s");
+                //console.log("scroll" + scrollD, "banner" + bannerHeight);
+                if (endDrop > scrollD && scrollD > bannerHeight) {
+                    theBall.css("transform", "translateY(" + ballDrop + "px)");
+                    theBall.css("z-index", layer);
+                } else if (scrollD > endDrop) {
+                    theBall.css("transform", "translateY(" + endDrop + "px)");
+                } else {
+                    theBall.css("transform", "translateY(0px)");
+                }
+            }
             
-            function bannerBall(targetBall, amount, layer){
-            let scrollD = $(window).scrollTop();
-            const windowHeight = $(window).height();
-            const bannerHeight = $("#bBanner").offset().top + windowHeight;
-            const theBall = $("#" + targetBall);
-            const endDrop = $("#website").offset().top - windowHeight;
-            let ballDrop = scrollD * amount;
-            theBall.css("transition", "1s");
-            if(endDrop > scrollD && scrollD > bannerHeight){
-                theBall.css("transform", "translateY(" + ballDrop + "px)");
-                theBall.css("z-index", layer);
-            }
-            else if(scrollD > endDrop){
-                theBall.css("transform", "translateY(" + endDrop + "px)");
-            }
-            else{
-                theBall.css("transform", "translateY(0px)");
-            }
-            }
+            
+
+            // nav bar function
+            //I guess svg code have to convert 2 times
+            // function showFullName(btnId) {
+            //     const targetBtn = document.getElementById(btnId);
+
+            //         targetBtn.style.display = "block";
+
+            // }
+            
+            // function hideFullName(btnId) {
+            //     const targetBtn = document.getElementById(btnId);
+            
+            //         targetBtn.style.display = "none";
+            // }
+
+            //targetBtn.css("animation", "none");
+            //targetBtn.css("animation", "btnbar 0.1s ease-out both");
