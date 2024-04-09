7cab19fdd5545c0d86bca5f038730f397a9d1960
Canada-Broccoli <chen0969@algonquinlive.com>
2024-04-08 23:07:41 -0400

    we2.0 update

---
+++ js/route.js
@@ -0,0 +1,49 @@
+// general
+function backHome(){
+    window.location.href = "index.html";
+}
+
+function seeAllJingAn(){
+    window.location.href = "all-jing-an.html"
+}
+
+// designer section
+function desPage(){
+    window.location.href = "designer.html"
+}
+
+function desFight(){
+    window.location.href = "designer-fight.html"
+}
+
+// developer section
+function devPage(){
+    window.location.href = "developer.html"
+}
+
+function devFight(){
+    window.location.href = "developer-fight.html"
+}
+
+// photographer section
+function phoPage(){
+    window.location.href = "photographer.html"
+}
+
+function phoFight(){
+    window.location.href = "photographer-fight.html"
+}
+
+// open social media link
+
+function openIG(){
+    window.open('https://www.instagram.com/huyohiya/');
+}
+
+function openLinkedIn(){
+    window.open('https://www.linkedin.com/in/broccolidesign');
+}
+
+function openWhatsApp(){
+    window.open('https://wa.me/13439884869');
+}
